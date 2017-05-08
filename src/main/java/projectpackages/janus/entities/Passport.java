85cb1385a7e2e60bea02e2abba7948bc4d2c498b
NikitaLipatov <Ovenvan@yandex.ru>
2017-05-08 15:34:42 +0300

    Начальная конфигурация проекта

--- src/main/java/projectpackages/janus/entities/Passport.java
+++ src/main/java/projectpackages/janus/entities/Passport.java
@@ -4,15 +4,32 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
-@Table(name = "passport")
 @Entity
+@Table(name = "passport")
 public class Passport {
 
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "uuid2")
-    @Column(name = "guid", unique = true, nullable = true)
+    @Column(name = "guid", unique = true, nullable = false)
     private String guid;
 
+    @Column(name = "animalType", nullable = false)
     private String animalType;
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
 }
